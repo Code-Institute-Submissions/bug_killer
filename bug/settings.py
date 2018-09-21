0363a39bd1b74739beb032cd0187f778a2db792e
Simon_Mac_Aree <smacr25@gmail.com>
2018-09-21 10:46:06 +0000

    adding a checkout app and stripe with stripe js in static

--- bug/settings.py
+++ bug/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'accounts',
     'products',
     'cart',
+    'checkout',
     'bug'
 ]
 
@@ -149,6 +150,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
+STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
--- bug/urls.py
+++ bug/urls.py
@@ -22,7 +22,8 @@ from django.views import static
 from accounts import urls as accounts_urls
 from products import urls as urls_products
 from cart import urls as urls_cart
-#from search import urls as urls_search
+from search import urls as urls_search
+from checkout import urls as urls_checkout
 from accounts.views import index
 from products.views import all_products
 from .settings import MEDIA_ROOT
@@ -36,6 +37,7 @@ urlpatterns = [
     url(r'^$', all_products, name='index'),
     url(r'^products/', include(urls_products)),
     url(r'^cart/', include(urls_cart)),
-#    url(r'^search/', include(urls_search)),
+    url(r'^checkout/', include(urls_checkout)),
+    url(r'^search/', include(urls_search)),
     url(r'^media/(?P<path>.*)$', static.serve,{'document_root': MEDIA_ROOT}),
 ]
